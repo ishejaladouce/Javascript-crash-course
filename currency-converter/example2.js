e39888be2a1c7b1ba9d39554f1990a9e697605dc
ishejaladouce <l.isheja@alustudent.com>
2025-09-18 14:28:58 +0200

    Add another example of currency conerter without for loop

---
+++ currency-converter/example2.js
@@ -0,0 +1,13 @@
+const currencies = {
+    usd: 1,
+    jpy: 113.5,
+    eur: 0.89,
+    rub: 74.36,
+    gbp: 0.75
+}
+console.log (`Welcome to currency converter`);
+console.log(`1 USD equals ${currencies.usd} USD`);
+console.log(`1 USD equals ${currencies.jpy} JPY`);
+console.log(`1 USD equals ${currencies.eur} EUR`);
+console.log(`1 USD equals ${currencies.rub} RUB`);
+console.log(`1 USD equals ${currencies.gbp} GBP`);
