3208c280e4b63c4f02924fcd787432d9a902eef8
ishejaladouce <l.isheja@alustudent.com>
2025-10-24 15:12:11 +0200

    Initial commit on numbers

---
+++ Meta-js-course/number.js
@@ -0,0 +1,11 @@
+//Numbers data type
+var a = 2
+console.log(a) // this will output 2
+
+//power
+ var b = 10**2
+ console.log (b) //this will output 100
+
+ //remainder
+var c = 9%8
+console.log (c) // this will output 1
