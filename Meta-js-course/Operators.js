bae89004872e0939518e8a094aebec8f15f25de1
ishejaladouce <l.isheja@alustudent.com>
2025-10-24 15:10:55 +0200

    Add short example on js operators

---
+++ Meta-js-course/Operators.js
@@ -0,0 +1,32 @@
+/* Arithmetic operators in js
+    + addition
+    - subtraction
+    * multiplication
+    / division
+*/
+
+/*Copmarison Operators
+    > greater that
+    < less than
+    == equal to
+*/
+
+//Examples on Arithmetic Operators
+console.log (2+2)
+console.log (4-2)
+console.log(4*5)
+console.log(16/4)
+
+//Examples on comparison Operators
+console.log (3>2)
+console.log (5<10)
+console.log (10 == 10)
+
+//Addition Assignment +=
+ var overtime = 1
+ overtime += 3
+ overtime += 2
+ overtime += 4
+ overtime += 6
+
+ console.log (overtime)
