b8c0b47a127745fa26627a42bff634dd85c72c2d
ishejaladouce <l.isheja@alustudent.com>
2025-10-24 15:12:43 +0200

    Modify file

--- Meta-js-course/greeting.js
+++ Meta-js-course/greeting.js
@@ -2,3 +2,17 @@ console.log("Hello World!")
 
 //Style console message
 console.log("%cHow have you been", "color:red; font-size:40px")
+
+var person = "John";
+
+console.log(person);
+
+var greeting = "Hello";
+console.log(greeting, person);
+
+//reassigning Variables
+
+var greeting = "Hi";
+var person = "James";
+
+console.log(greeting, person);
