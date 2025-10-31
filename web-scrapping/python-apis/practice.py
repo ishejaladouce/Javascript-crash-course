f5778af5236e8de4daf47376f40ce7ba2a1324f2
ishejaladouce <l.isheja@alustudent.com>
2025-10-31 14:14:00 +0200

    Delete and re-create file to count all users and theor posts

--- web-scrapping/python-apis/practice.py
+++ web-scrapping/python-apis/practice.py
@@ -1,12 +1,10 @@
 import json
 
-with open("users.json", "r") as users_file:
-    users = json.load(users_file)
-
 with open("posts.json", "r") as posts_file:
     posts = json.load(posts_file)
 
-print(f"There are {len(users)} users.\n")
+with open("users.json", "r") as users_file:
+    users = json.load(users_file)
 
 for user in users:
     user_id = user["id"]
@@ -15,8 +13,11 @@ for user in users:
     user_posts = [post for post in posts if post["userId"] == user_id]
 
     print(f"User {user_name} has {len(user_posts)} posts")
-    if user_posts:  
-        print("Her/His posts are:")
+
+    if user_posts:
+        print("Posts made")
+
         for i, post in enumerate(user_posts, start=1):
-            print(f"  {i}. {post['title']}")
-    print("\n") 
+            print(f" {i}. {post['title']}")
+
+    print("\n")
