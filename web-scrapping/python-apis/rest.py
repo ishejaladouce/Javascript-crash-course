40fa5acdeb4abe406af983332eb64c80a98264c2
ishejaladouce <l.isheja@alustudent.com>
2025-11-05 12:04:54 +0200

    Add a practice file to calculate the total posts, total posts done by Leanne, ad their titles

--- web-scrapping/python-apis/rest.py
+++ web-scrapping/python-apis/rest.py
@@ -1,13 +1,38 @@
-# Count posts made by user id 1 (Lean)
-# Print the title of each post
-import json
+import requests
 
-with open("posts.json", "r") as file:
-    posts = json.load(file)
+user_response = requests.get("https://jsonplaceholder.typicode.com/users")
+post_response = requests.get("https://jsonplaceholder.typicode.com/posts")
 
-counter = 0
-for item in posts:
-    if item["userId"] == 1:
-        counter += 1
-print(counter)
-    
+users = user_response.json()
+posts = post_response.json()
+
+# print(f"{posts}")
+
+#  find Leanne's Id
+
+Leanne_id = None
+for user in users:
+    if user["name"] == "Leanne Graham":
+        Leanne_id = user["id"]
+        break
+
+# Count total post 
+total_posts = len(posts)
+
+print(f"The total posts made are: {total_posts}")
+
+#count total posts made by Leanne
+Leanne_posts = []
+for post in posts:
+    if post["userId"] == Leanne_id:
+        Leanne_posts.append(post)
+her_posts = len(Leanne_posts)
+
+print(f"The total posts done by Leanne are: {her_posts}")
+
+#print the title of posts done by Leanne
+
+counter = 1
+for post in Leanne_posts:
+    print(f"\t{counter}. {post['title']}")
+    counter += 1
