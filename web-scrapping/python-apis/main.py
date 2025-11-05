cf320af5e0e1f25d98357609f650989f5f9dd63a
ishejaladouce <l.isheja@alustudent.com>
2025-11-05 11:24:50 +0200

    Print Leanne's post titles

--- web-scrapping/python-apis/main.py
+++ web-scrapping/python-apis/main.py
@@ -14,8 +14,30 @@ for user in users:
 
 #test  the codes if fetching the id for Leanne
 print("Leanne Graham user id: ", Leanne_id)
+print("")
 
-# count total posts
+# count total "posts"
 total_posts = len(posts)
 print("Total posts: ", total_posts)
+print("")
 
+#Count total posts done by Leanne
+Leanne_posts = []
+
+for post in posts:
+    if post["userId"] == Leanne_id:
+        Leanne_posts.append(post)
+her_posts = len(Leanne_posts)
+
+#test
+print(f"Leanne Graham has {her_posts} posts.")
+print("")
+
+# Print Leanne's post titles
+print("\t Leanne's post's are: ")
+print("\t --------------------")
+
+counter = 1
+for post in Leanne_posts:
+    print(f" \t {counter}. {post['title']}")
+    counter += 1
